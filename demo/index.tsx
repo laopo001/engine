d1dfabd9ed2e3fdec6c76e14307cb92d38b2a414
laopo001 <353272497@qq.com>
2018-04-10 00:58:08 +0800

    fix

--- demo/index.tsx
+++ demo/index.tsx
@@ -7,7 +7,7 @@ var canvas = document.getElementById("root");
 
 hpc.render(<Application>
     <Entity name='123'  >
-        <camera clearColor={new pc.Color(0.12, 0.12, 0.12)} />
+        <camera clearColor={new pc.Color(0.12, 0.12, 0.12)}></camera>
     </Entity>
     <Entity >
         <model type='123' />
