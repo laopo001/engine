ed4e0aa83a865261e7578b971672a352f3d1d0f5
laopo001 <353272497@qq.com>
2018-04-25 01:58:00 +0800

    fix

--- package/index.ts
+++ package/index.ts
@@ -1,6 +1,10 @@
+/// <reference path="../typings/index.d.ts" />
+
 import { Application } from './application.tag';
 import { HpcComponent } from './script_commponent';
 import { Entity } from './entity.tag';
+import { Node } from './node';
+import { pc } from './playcanvas/playcanvas-latest.js'
 // import { loadAssetsFromUrl, createMaterial, randomRange, randomEnum, once } from './util';
 import { h } from './h';
 import { render } from './render';
@@ -8,8 +12,9 @@ const hpc = {
     h
 }
 window['hpc'] = hpc;
+window['pc'] = pc;
 export {
-    render, h, Application, Entity, HpcComponent
+    render, h, Application, Entity, HpcComponent, pc
 };
 export default hpc;
 export * from './util';
